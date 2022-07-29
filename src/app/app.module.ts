e040f5c820a003e36225d9678988401bd474434d
eduardo_chavez <eduardo.dorado@sanmina.com>
2022-07-29 10:22:06 -0500

    create - page, component directories and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Pages1Component } from './pages/pages1/pages1.component';
+import { ShowNameComponent } from './components/show-name/show-name.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Pages1Component,
+    ShowNameComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/pages1/pages1.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pages1',
+  templateUrl: './pages1.component.html',
+  styles: [
+  ]
+})
+export class Pages1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
